b206d42ad892f5e7c2f872902b6cca36c7b56014
Andrés García <andgar2010@gmail.com>
2022-12-14 04:23:05 -0500

    Revert "remove: override file googletagmanager and partytown"
    This reverts commit b2535671fe5872244540bb152c82bd2755d12012.

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+import { partytownSnippet } from "@builder.io/partytown/integration";
+
+const snippetContent = partytownSnippet();
+const script = document.createElement("script");
+
+script.textContent = snippetContent;
+document.body.append(script);
