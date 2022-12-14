5525fbfef1b4e6ab5b118d24d75b43a5f361d69e
Andrés García <andgar2010@gmail.com>
2022-12-14 03:20:22 -0500

    feat: add asset js index

---
+++ assets/js/index.js
@@ -0,0 +1,4 @@
+export * from './~partytown/partytown.js';
+export * from './~partytown/partytown-sw.js';
+export * from './~partytown/partytown-media.js';
+export * from './~partytown/partytown-atomics.js';
