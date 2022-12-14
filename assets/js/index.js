b2535671fe5872244540bb152c82bd2755d12012
Andrés García <andgar2010@gmail.com>
2022-12-14 04:23:05 -0500

    remove: override file googletagmanager and partytown

--- assets/js/index.js
+++
@@ -1,7 +0,0 @@
-import { partytownSnippet } from '@builder.io/partytown/integration';
-
-const snippetContent = partytownSnippet();
-const script = document.createElement("script");
-
-script.textContent=snippetContent;
-document.body.append(script)
