c37f3e5b120ee85b727a8cb2df7e80fdb83f8641
andgar2010 <andgar2010@users.noreply.github.com>
2023-03-29 21:07:13 +0000

    deploy: 2548615c0342ccd2083cf379143cefa47aa441cd

--- js/cursor_color_blur.js
+++
@@ -1,1 +0,0 @@
-(()=>{var n=document.getElementById("cursor-color");document.body.onpointermove=o=>{let{clientX:t,clientY:e}=o;n.animate({left:`${t}px`,top:`${e}px`},{duration:1e3,fill:"forwards"})};})();
