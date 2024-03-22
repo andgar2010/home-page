2027811f8fe057fcf5dc32587fc3ad447cd8690d
Andres Garcia (TECH-ANDGAR) <dev@tech-andgar.me>
2024-03-21 19:09:41 -0500

    refactor(Cursor Color Blur): Improve cursor movement handling
    Replaced onpointermove event with window's mousemove event for better compatibility. Also, adjusted the calculation of x and y coordinates to center the cursor color blur effect.

--- assets/js/cursor_color_blur.js
+++ assets/js/cursor_color_blur.js
@@ -1,9 +1,15 @@
 // Cursor Color Blur
-const cursorColor = document.getElementById("cursor-color-bg")
-document.body.onpointermove = (event) => {
+const cursorColor = document.getElementById("cursor-color-bg");
+
+window.addEventListener("mousemove", (e) => {
   const { clientX, clientY } = event;
+
+  const x = clientX - cursorColor.offsetWidth / 2,
+    y = clientY - cursorColor.offsetHeight / 2;
+
   cursorColor.animate({
-    left: `${clientX}px`,
-    top: `${clientY}px`,
+    left: `${x}px`,
+    top: `${y}px`,
   }, { duration: 1000, fill: "forwards" });
-};
+
+});
